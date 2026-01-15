fba090e1ecea442e7c363eb37ba26a158d46f6d9
Umberto Gotti <umberto.gotti@umbertogotti.dev>
2026-01-15 07:59:29 +0100

    feat: network partitioning implemented

--- raft/sim/src/test_cluster.rs
+++ raft/sim/src/test_cluster.rs
@@ -3,7 +3,10 @@
 // http://www.apache.org/licenses/LICENSE-2.0
 
 use crate::{
-    in_memory_log_entry_collection::InMemoryLogEntryCollection, in_memory_map_collection::InMemoryMapCollection, in_memory_state_machine::InMemoryStateMachine, in_memory_storage::InMemoryStorage, in_memory_transport::InMemoryTransport, message_broker::MessageBroker, vec_node_collection::VecNodeCollection
+    in_memory_log_entry_collection::InMemoryLogEntryCollection,
+    in_memory_map_collection::InMemoryMapCollection, in_memory_state_machine::InMemoryStateMachine,
+    in_memory_storage::InMemoryStorage, in_memory_transport::InMemoryTransport,
+    message_broker::MessageBroker, vec_node_collection::VecNodeCollection,
 };
 use indexmap::IndexMap;
 use raft_core::{
@@ -19,13 +22,14 @@ type InMemoryRaftNode = RaftNode<
     InMemoryStateMachine,
     VecNodeCollection,
     InMemoryLogEntryCollection,
-    InMemoryMapCollection
+    InMemoryMapCollection,
 >;
 
 pub struct TestCluster {
     nodes: IndexMap<NodeId, InMemoryRaftNode>,
     broker: Arc<Mutex<MessageBroker<String, InMemoryLogEntryCollection>>>,
     message_log: Vec<(NodeId, NodeId, RaftMsg<String, InMemoryLogEntryCollection>)>,
+    partitions: Option<(Vec<NodeId>, Vec<NodeId>)>,
 }
 
 impl TestCluster {
@@ -34,6 +38,7 @@ impl TestCluster {
             nodes: IndexMap::new(),
             broker: Arc::new(Mutex::new(MessageBroker::new())),
             message_log: Vec::new(),
+            partitions: None,
         }
     }
 
@@ -145,6 +150,57 @@ impl TestCluster {
 
             let node = self.nodes.get_mut(&to).unwrap();
             node.on_event(Event::Message { from: sender, msg });
+        }
+    }
+
+    /// Partition the cluster into two groups
+    /// Messages can only be delivered within each partition
+    pub fn partition(&mut self, partition1: &[NodeId], partition2: &[NodeId]) {
+        self.partitions = Some((partition1.to_vec(), partition2.to_vec()));
+    }
+
+    /// Heal the partition - allow messages between all nodes
+    pub fn heal_partition(&mut self) {
+        self.partitions = None;
+    }
+
+    /// Deliver messages only within a specific partition
+    pub fn deliver_messages_partition(&mut self, partition: &[NodeId]) {
+        let mut delivered_any = true;
+        let mut rounds = 0;
+        const MAX_ROUNDS: usize = 10;
+
+        while delivered_any && rounds < MAX_ROUNDS {
+            delivered_any = false;
+            rounds += 1;
+
+            // Collect messages within this partition
+            let mut messages_to_deliver = Vec::new();
+            {
+                let mut broker = self.broker.lock().unwrap();
+                for &node_id in partition {
+                    // Only deliver messages if both sender and receiver are in partition
+                    while let Some((from, msg)) = broker.dequeue(node_id) {
+                        if partition.contains(&from) {
+                            messages_to_deliver.push((node_id, from, msg));
+                        }
+                        // If sender not in partition, message is dropped (simulates network partition)
+                    }
+                }
+            }
+
+            if messages_to_deliver.is_empty() {
+                break;
+            }
+
+            // Deliver collected messages
+            for (node_id, from, msg) in messages_to_deliver {
+                self.message_log.push((from, node_id, msg.clone()));
+
+                let node = self.nodes.get_mut(&node_id).unwrap();
+                node.on_event(Event::Message { from, msg });
+                delivered_any = true;
+            }
         }
     }
 }
